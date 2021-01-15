7c5150692c68c1cc48b99bfaba048406c7e7d198
Jonathan Hook <jonathan.hook+github@gmail.com>
2021-01-15 10:55:28 +0000

    Project working

---
+++ Assets/Solution/TrackController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackController : MonoBehaviour
+{
+    public int TrackToControl = 0;
+    public DJMixer mixer;
+
+    public void OnTargetFound()
+    {
+        Debug.Log(TrackToControl + " found");
+        mixer.SetTrackState(TrackToControl, true);
+    }
+
+    public void OnTargetLost()
+    {
+        Debug.Log(TrackToControl + " lost");
+        mixer.SetTrackState(TrackToControl, false);
+    }
+}
